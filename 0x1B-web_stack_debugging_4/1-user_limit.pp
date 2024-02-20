3c7eb60c2b7546a3c918735b9422d9b90b12875e
Hab10000 <Alazarembakom198@gmail.com>
2024-02-20 10:07:51 +0000

    add

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,7 @@
+
+gin with the holberton user and open a file without any error message.
+file { 'loginFile':
+    ensure  => present,
+    path    => '/etc/security/limits.conf',
+    content => '#File erased'
+}
